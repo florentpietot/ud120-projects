514104d607895eef7e8f7fc525ca0add703fdcd5
Florent Pietot <florent.pietot@gmail.com>
2018-05-29 17:35:48 +0200

    Finish Choose your own algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -28,15 +28,20 @@ plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.metrics import accuracy_score
 
+# clf = KNeighborsClassifier(n_neighbors=5)
+clf = RandomForestClassifier()
 
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
-
-
-
+acc = accuracy_score(labels_test, pred)
+print "Accuracy for {}: {}".format(clf.__class__.__name__, acc)
 
 try:
     prettyPicture(clf, features_test, labels_test)
